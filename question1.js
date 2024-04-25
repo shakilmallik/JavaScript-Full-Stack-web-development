c40d918e36ac81f62967d09c5c0f48b20ce03d2e
Shakil Anowar Mallik <shakilmallik782@gmail.com>
2024-04-25 12:52:50 +0530

    initial comment

---
+++ question1.js
@@ -0,0 +1,5 @@
+let totalValue = 2000;
+let discountPercentage = 20;
+
+let discountPrice = totalValue - (discountPercentage/100)*totalValue;
+console.log("After Discount the final price is: Rs." ,discountPrice);
