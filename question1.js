f596dae4763b5f3f65d28c53e1774a7d4f22c0da
Shakil Anowar Mallik <shakilmallik782@gmail.com>
2024-04-25 13:19:13 +0530

    add the question

--- question1.js
+++ question1.js
@@ -1,3 +1,4 @@
+//q1. Suppose you are building a payment checkout page and must display the final price after the discount.Create a simple discount calculator that take two values from the variables- the total cost and the discount percentage - and print the discounted value.
 let totalValue = 2000;
 let discountPercentage = 20;
 
